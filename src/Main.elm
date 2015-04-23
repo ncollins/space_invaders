dddfb3f775842a4191918e549a774768b3f85d2e
Nick Collins <nick@vividx.co.uk>
2015-04-23 19:33:12 -0400

    updates for Elm Platform 0.15

--- src/Main.elm
+++ src/Main.elm
@@ -2,8 +2,10 @@ import Time
 import Signal
 import Keyboard
 import List
-import Graphics.Collage
+import Graphics.Collage exposing (..)
+import Graphics.Element
 import Text
+import Color
 
 -- CONSTANTS ----------------------------------
 
@@ -14,40 +16,40 @@ statusHeight = 30
 debugHeight = 30
 totalHeight = gameHeight + statusHeight + debugHeight
 
-bgColor = black
+bgColor = Color.black
 ticksPerSecond = 20
 tickPeriod = Time.second / 20 -- 10/sec
 
 -- STATE ----------------------------------
 
-data State = GameOver Int | Game GameState
+type State = GameOver Int | Game GameState
 
-type GameState = { invaders: [Invader]
+type alias GameState = { invaders: List Invader
                  , player: Player
-                 , bullets: [Bullet]
-                 , explosions: [Explosion]
-                 , time: Time
+                 , bullets: List Bullet
+                 , explosions: List Explosion
+                 , time: Time.Time
                  , input: Input
                  , score: Int
                  , lives: Int }
 
-type Invader = { x: Float, y: Float, dx: Float, dy: Float, points: Int }
+type alias Invader = { x: Float, y: Float, dx: Float, dy: Float, points: Int }
 
-type Player = { x: Float, y: Float, dx: Float, dy: Float }
+type alias Player = { x: Float, y: Float, dx: Float, dy: Float }
 
-type Bullet = { x: Float, y: Float, dx: Float, dy: Float, shotAt: Time }
+type alias Bullet = { x: Float, y: Float, dx: Float, dy: Float, shotAt: Time.Time }
 
-type Explosion = { x: Float, y: Float, alpha: Float }
+type alias Explosion = { x: Float, y: Float, alpha: Float }
 
-invaders = map (\n -> { x = n*unitWidth - 250, y = 200, dx = 10, dy = 0 , points = 10}) [1..8]
-           ++ (map (\n -> { x = n*unitWidth - 250, y = 240, dx = 10, dy = 0 , points = 20}) [1..8])
+invaders = List.map (\n -> { x = n*unitWidth - 250, y = 200, dx = 10, dy = 0 , points = 10}) [1..8]
+           ++ (List.map (\n -> { x = n*unitWidth - 250, y = 240, dx = 10, dy = 0 , points = 20}) [1..8])
 
 player : Player
 player = { x = 0, y = -200, dx = 0, dy = 0 }
 
 start : GameState
 start = { invaders = invaders
-        ,  player = player
+        , player = player
         , bullets = []
         , explosions = []
         , time = 0.0
@@ -59,18 +61,21 @@ start = { invaders = invaders
 
 inGameArea e = (abs e.x < gameWidth/2) && abs e.y < gameHeight/2
 
-moveInvader : Time -> Float -> Invader -> Invader
+moveInvader : Time.Time -> Float -> Invader -> Invader
 moveInvader time direction i =
   { i | x <- i.x + i.dx * time, dx <- i.dx * direction }
 
-moveInvaders : Time -> [Invader] -> [Invader]
+moveInvaders : Time.Time -> List Invader -> List Invader
 moveInvaders time invaders = 
-  let l = (head invaders)
-      r = (last invaders)
-  in
-  if | l.x < -200 && l.dx < 0  -> map (moveInvader time -1.0) invaders
-     | r.x > 200 && r.dx > 0   -> map (moveInvader time -1.0) invaders
-     | otherwise               -> map (moveInvader time 1.0) invaders
+  case invaders of
+    [] -> []
+    l::vs ->
+      case vs of
+        [] -> [l]
+        r::_ ->  
+            if | l.x < -200 && l.dx < 0  -> List.map (moveInvader time -1.0) invaders
+               | r.x > 200 && r.dx > 0   -> List.map (moveInvader time -1.0) invaders
+               | otherwise               -> List.map (moveInvader time 1.0) invaders
 
 
 movePlayer : Input -> Player -> Player
@@ -79,15 +84,14 @@ movePlayer input player =
   in
   { player | x <- bound <| player.x + 10 * toFloat input.x }
 
-updateExplosions : [Explosion] -> [Explosion]
-updateExplosions es = map (\e -> { e | alpha <- max 0 (e.alpha - 0.05) }) es
+updateExplosions : List Explosion -> List Explosion
+updateExplosions es = List.map (\e -> { e | alpha <- max 0 (e.alpha - 0.05) }) es
   
-updateBullets : Float -> GameState -> Input -> [Bullet] -> [Bullet]
+updateBullets : Float -> GameState -> Input -> List Bullet -> List Bullet
 updateBullets interval s i bs =
   let dy = 100
       move b = {b | y <- b.y + interval * b.dy }
-      someBullets = not (List.isEmpty bs)
-      lastShot = if someBullets  then Just (head bs).shotAt else Nothing
+      lastShot = Maybe.map (\h -> h.shotAt) (List.head bs)
       newBullet = case (i.space, lastShot) of
                         (False, _)      -> Nothing
                         (True, Just t)  -> if (i.time - t) < 500 -- MAGIC
@@ -97,8 +101,8 @@ updateBullets interval s i bs =
                         (True, Nothing) -> Just { x = s.player.x, y = s.player.y
                                                 , dx = 0, dy = dy, shotAt = i.time }
   in case newBullet of
-     Nothing -> (filter inGameArea . map move) bs
-     Just b -> (filter inGameArea . map move) (b::bs)
+     Nothing -> (List.filter inGameArea >> List.map move) bs
+     Just b -> (List.filter inGameArea >> List.map move) (b::bs)
      
 hit e1 e2 = abs (e1.x - e2.x) < 20 && abs (e1.y - e2.y) < 10 -- MAGIC
 
@@ -109,8 +113,8 @@ updateGame input state =
       (deadVader, liveVader) = if List.isEmpty state.invaders
                                then ([],[])
                                else List.partition (\v -> List.any (hit v) bullets) (moveInvaders interval state.invaders)
-      explosions = let old = (filter (\e -> e.alpha > 0) state.explosions)
-                       new = map (\i -> { x = i.x, y = i.y, alpha = 1 }) deadVader
+      explosions = let old = (List.filter (\e -> e.alpha > 0) state.explosions)
+                       new = List.map (\i -> { x = i.x, y = i.y, alpha = 1 }) deadVader
                    in (updateExplosions old) ++ new
   in
   { state | player <- movePlayer input state.player
@@ -119,7 +123,7 @@ updateGame input state =
           , input <- input 
           , bullets <- bullets
           , explosions <- explosions
-          , score <- state.score + sum (map (\i -> i.points) deadVader) }
+          , score <- state.score + List.sum (List.map (\i -> i.points) deadVader) }
 
 update : Input -> State -> State
 update input state =
@@ -133,19 +137,19 @@ update input state =
 
 -- INPUT ----------------------------------
 
-type Input = { x: Int, y: Int, space: Bool
-             , counter: Int, time: Time}
+type alias Input = { x: Int, y: Int, space: Bool
+             , counter: Int, time: Time.Time}
 
-counter = foldp (\_ c -> c + 1) 0 (Time.every tickPeriod)
-time = Time.timestamp counter |> lift fst
+counter = Signal.foldp (\_ c -> c + 1) 0 (Time.every tickPeriod)
+time = Time.timestamp counter |> Signal.map fst
 
-combineInput : { x: Int, y: Int} -> Bool -> Int -> Time -> Input
+combineInput : { x: Int, y: Int} -> Bool -> Int -> Time.Time -> Input
 combineInput arrows space counter time =
   { x = arrows.x, y = arrows.y, space = space
   , counter = counter, time = time }
 
 input : Signal Input
-input = lift4 combineInput Keyboard.arrows Keyboard.space counter time
+input = Signal.map4 combineInput Keyboard.arrows Keyboard.space counter time
 
 -- DISPLAY ----------------------------------
 
@@ -153,9 +157,9 @@ input = lift4 combineInput Keyboard.arrows Keyboard.space counter time
 displayInvader : Invader -> Form
 displayInvader i = 
     case i.points of
-        10 -> move (i.x, i.y) (invader red bgColor)
-        20 -> move (i.x, i.y) (invader blue bgColor)
-        _  -> move (i.x, i.y) (invader purple bgColor)
+        10 -> move (i.x, i.y) (invader Color.red bgColor)
+        20 -> move (i.x, i.y) (invader Color.blue bgColor)
+        _  -> move (i.x, i.y) (invader Color.purple bgColor)
 
 displayExplosion : Explosion -> Form
 displayExplosion e =
@@ -164,39 +168,39 @@ displayExplosion e =
 displayPlayer : Player -> Form
 displayPlayer p = let translate = move (p.x, p.y)
                       turn = rotate (degrees 90)
-                  in (translate . turn) (filled blue (ngon 3 20))
+                  in (translate >> turn) (filled Color.blue (ngon 3 20))
 
-displayBullet b = move (b.x, b.y) (filled green (rect 2 10))
+displayBullet b = move (b.x, b.y) (filled Color.green (rect 2 10))
 
 displayGameStatus top s =
-    let background = filled lightGreen (rect 500 30)
-        text = (Graphics.Collage.toForm . asText) s.score
-    in map (move (0, top)) [background, text]
+    let background = filled Color.lightGreen (rect 500 30)
+        text = (Graphics.Element.show >> Graphics.Collage.toForm) s.score
+    in List.map (move (0, top)) [background, text]
 
 displayDebugInfo top s =
-    let background = filled grey (rect 500 30)
-        text = (Graphics.Collage.toForm . asText) s.input
-    in map (move (0, top)) [background, text]
+    let background = filled Color.grey (rect 500 30)
+        text = (Graphics.Element.show >> Graphics.Collage.toForm) s.input
+    in List.map (move (0, top)) [background, text]
 
-displayGame : GameState -> Element
-displayGame s = let is = map displayInvader s.invaders
+displayGame : GameState -> Graphics.Element.Element
+displayGame s = let is = List.map displayInvader s.invaders
                     p = displayPlayer s.player
-                    bullets = map displayBullet s.bullets
-                    explosions = map displayExplosion s.explosions
+                    bullets = List.map displayBullet s.bullets
+                    explosions = List.map displayExplosion s.explosions
                     background = filled bgColor (rect gameWidth totalHeight)
                     status = displayGameStatus (45 - totalHeight/2) s
                     debug = displayDebugInfo (15 - totalHeight/2) s 
                 in collage gameWidth totalHeight <| [background, p] ++ is ++ bullets ++ explosions ++ status ++ debug
 
-display : State -> Element
+display : State -> Graphics.Element.Element
 display state =
     case state of
-        GameOver score -> asText score
+        GameOver score -> Graphics.Element.show score
         Game s   -> displayGame s
 
 -- MAIN ----------------------------------
 
-main = lift display (foldp update (Game start) input)
+main = Signal.map display (Signal.foldp update (Game start) input)
 
 -- VISUAL
 
@@ -210,14 +214,14 @@ invader fg bg = group [(filled fg (oval 50 40))
                       ] |> Graphics.Collage.scale (4/5)
 
 explosion =
-    let reds = [filled red (polygon [(0, -5), (0, 5), (30, 0)])
-               , filled red (polygon [(0, 5), (-30, 10), (20, -20)])
-               ,filled red (polygon [(10, 0), (10, 20), (-30, -20)])
+    let reds = [filled Color.red (polygon [(0, -5), (0, 5), (30, 0)])
+               , filled Color.red (polygon [(0, 5), (-30, 10), (20, -20)])
+               ,filled Color.red (polygon [(10, 0), (10, 20), (-30, -20)])
                ]
-        yellows = [filled yellow (polygon [(0, -5), (0, 5), (30, 0)])
-                  , filled yellow (polygon [(0, 5), (-30, 10), (20, -20)])
-                  ,filled yellow (polygon [(10, 0), (10, 20), (-30, -20)])
+        yellows = [filled Color.yellow (polygon [(0, -5), (0, 5), (30, 0)])
+                  , filled Color.yellow (polygon [(0, 5), (-30, 10), (20, -20)])
+                  ,filled Color.yellow (polygon [(10, 0), (10, 20), (-30, -20)])
                   ]
-        rot120 = map (rotate (degrees 125))
-        scale60 = map (scale 0.6)
+        rot120 = List.map (rotate (degrees 125))
+        scale60 = List.map (scale 0.6)
     in (rot120 (reds ++ (scale60 yellows))) ++ reds ++ (scale60 yellows) |> group |> Graphics.Collage.scale (4/5)
